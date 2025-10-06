e564b83548fd4cafe88fe4c7b95a7749b125129f
Robin313k <107352159+Robin313k@users.noreply.github.com>
2025-10-06 07:17:47 +0000

    Added a register to store data for an unlimited time frame

--- main.c
+++ main.c
@@ -5,32 +5,39 @@
 typedef enum {
 	OP_HALT = 0,
     	OP_PUSH,     		// Push a constant onto the stack
-    	OP_DEL,	 		// Delete latest constant of the stack
+    	OP_DEL,	 			// Delete latest constant of the stack
     	OP_ADD,      		// Pop two values, add them, push result
-    	OP_SUB,	 		// Pop two calues, subtract one from the other, push result
-    	OP_MULT, 	 	// Pop two values, multiply them with each other, push result
-    	OP_DIV,	 		// Pop two values, divide one through the other, push result
+    	OP_SUB,	 			// Pop two calues, subtract one from the other, push result
+    	OP_MULT, 	 		// Pop two values, multiply them with each other, push result
+    	OP_DIV,	 			// Pop two values, divide one through the other, push result
     	OP_PRINT,    		// Print top of stack and consume it
-    	OP_PEEK,	 	// Print top of stack without consuming it
-    	OP_JMP,	 		// Jump to the instruction matching the given index
+    	OP_PEEK,	 		// Print top of stack without consuming it
+    	OP_JMP,	 			// Jump to the instruction matching the given index
     	OP_JMP_IF_ZERO,		// Jump if top of stack does equal zero
     	OP_JMP_IF_NOT_ZERO,	// Jump if top of stack does not equal zero
-    	OP_EQ,			// Pop top two values, push 1 if equal, push 0 if not
-    	OP_NOT_EQ,		// Pop top two values, push 1 if not equal, push 0 if equal
-    	OP_LT,    		// Pop top two values, push 1 if less than, else push 0
-    	OP_GT,    		// Pop top two values, push 1 if greater than, else push 0
-    	OP_LE,    		// Pop top two values, push 1 if greater than or equal, else push 0
-    	OP_GE,    		// Pop top two values, push 1 if greater than or equal, else push 0
-	OP_DUP,			// Pop top value and push it twice
-	OP_SWAP,		// Swap top 2 values
-	OP_OVER,		// Copy second element on top 4, 8 -> 4, 8, 4
-	OP_NOP			// Empty instruction
+    	OP_EQ,				// Pop top two values, push 1 if equal, push 0 if not
+    	OP_NOT_EQ,			// Pop top two values, push 1 if not equal, push 0 if equal
+    	OP_LT,    			// Pop top two values, push 1 if less than, else push 0
+    	OP_GT,    			// Pop top two values, push 1 if greater than, else push 0
+    	OP_LE,    			// Pop top two values, push 1 if greater than or equal, else push 0
+    	OP_GE,    			// Pop top two values, push 1 if greater than or equal, else push 0
+		OP_DUP,				// Pop top value and push it twice
+		OP_SWAP,			// Swap top 2 values
+		OP_OVER,			// Copy second element on top 4, 8 -> 4, 8, 4
+		OP_NOP,				// Empty instruction
+
+		OP_STORE_R,			//Pop top value and store in given register address
+		OP_LOAD_R			//Pop value of given register address and load it onto the stack
 } OpCode;
 
 // Stack setup
 #define STACK_SIZE 256
 int stack[STACK_SIZE];
 int sp = -1; // Stack pointer
+
+// Register setup
+#define REGISTER_SIZE 8
+uint16_t reg[REGISTER_SIZE]; // R0-R7
 
 // Stack operations
 void push(int value) {
@@ -65,60 +72,62 @@ void run(uint8_t *program, size_t length) {
         	OpCode op = program[pc++];  // Fetch
 
         	switch (op) {
-            		case OP_HALT:
-                		return;
+            	case OP_HALT: {
+            		return;
+				}
 
-            		case OP_PUSH:
-                		push(program[pc++]);
-                		break;
+            	case OP_PUSH: {
+            		push(program[pc++]);
+            		break;
+				}
 
 	    		case OP_DEL: {
 	    			int a = pop();
 				break;
 	    		}
 
-            		case OP_ADD: {
-                		int b = pop();
-                		int a = pop();
-                		push(a + b);
-                		break;
-            		}
+            	case OP_ADD: {
+            		int b = pop();
+            		int a = pop();
+            		push(a + b);
+            		break;
+        		}
 
 	    		case OP_SUB: {
-				int b = pop();
-				int a = pop();
-				push(a - b);
-				break;		
+					int b = pop();
+					int a = pop();
+					push(a - b);
+					break;		
 	    		}
 
 	    		case OP_MULT: {
-				int b = pop();
-				int a = pop();
-				push(a * b);
-				break;		
+					int b = pop();
+					int a = pop();
+					push(a * b);
+					break;		
 	    		}
 
 	    		case OP_DIV: {
-				int b = pop();
-				int a = pop();
-				if (b == 0) {
-					printf("You can NOT divide by 0.\nThis VM will shut down as means of a security measure!\n");
-					return;
-				}
+					int b = pop();
+					int a = pop();
+					if (b == 0) {
+						printf("You can NOT divide by 0.\nThis VM will shut down as means of a security measure!\n");
+						return;
+					}
 
-				push(a / b);		
-				break;
+					push(a / b);		
+					break;
 	    		}
 
-            		case OP_PRINT: {
-                		printf("%d\n", pop());
-                		break;
-			}
+            	case OP_PRINT: {
+                	printf("%d\n", pop());
+                	break;
+				}
 
 	    		case OP_PEEK: {
 				printf("%d\n", peek());
 				break;
-			}
+				}
 
 	    		case OP_JMP: {
            			if (pc >= length) {
@@ -128,7 +137,7 @@ void run(uint8_t *program, size_t length) {
 
     				pc = program[pc];  // Jump to absolute position
     				break;
-			}
+				}
 
 	    		case OP_JMP_IF_ZERO: {
            			if (pc >= length) {
@@ -136,12 +145,15 @@ void run(uint8_t *program, size_t length) {
         				return;
     				}
 
-				if (peek() == 0) {
-    					pc = program[pc];  // Jump to absolute position
-				}
+					uint8_t target = program[pc++];
+					int condition = pop();
 
-				break;
-			}
+					if (condition == 0) {
+    					pc = target;
+					}
+
+					break;
+				}
 
 	    		case OP_JMP_IF_NOT_ZERO: {
            			if (pc >= length) {
@@ -152,24 +164,24 @@ void run(uint8_t *program, size_t length) {
 				uint8_t target = program[pc++];
 				int condition = pop();
 
-				if (condition != 0) {
+					if (condition != 0) {
     					pc = target;
+					}
+
+					break;
 				}
 
-				break;
-			}
+				case OP_EQ: {
+					int b = pop();	    
+					int a = pop();	    
+					if (a == b) {
+						push(1);
+					} else {
+						push(0);
+					}
 
-			case OP_EQ: {
-				int b = pop();	    
-				int a = pop();	    
-				if (a == b) {
-					push(1);
-				} else {
-					push(0);
+				 	break;
 				}
-
-				 break;
-			}
 
 			case OP_NOT_EQ: {
 				int b = pop();	    
@@ -261,6 +273,16 @@ void run(uint8_t *program, size_t length) {
 				break;
 			}
 
+				case OP_STORE_R: {
+					reg[program[pc++]] = pop();
+					break;
+				}
+
+				case OP_LOAD_R: {
+					push(reg[program[pc++]]);
+					break;
+				}
+
 			default: {
                 		printf("Unknown instruction: %d\n", op);
                 		return;
@@ -273,15 +295,20 @@ void run(uint8_t *program, size_t length) {
 int main() {
     	// Example bytecode: push 2, push 3, add, print, halt
     	uint8_t program[] = {
-		OP_PUSH, 8,			
-		OP_PUSH, 64,
-		OP_PEEK,
-		OP_OVER,
-		OP_PRINT,
-    		OP_HALT				
+			OP_PUSH, 8,
+			OP_DUP,
+			OP_DUP,
+			OP_MULT,
+			OP_PRINT,
+			OP_DUP,
+			OP_PUSH, 1,
+			OP_SUB,
+			OP_DUP,
+			OP_JMP_IF_NOT_ZERO, 2,
+			OP_HALT
 	};
 
 
-    	run(program, sizeof(program));
-    	return 0;
+    run(program, sizeof(program));
+	return 0;
 }
