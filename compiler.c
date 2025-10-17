a4ce753ffc31138f30473c483413434a74a7135e
Robin313k <107352159+Robin313k@users.noreply.github.com>
2025-10-17 16:50:11 +0000

    register were added and more
    idk, I cant remember more than adding registers. Good luck!

--- compiler.c
+++ compiler.c
@@ -5,97 +5,122 @@
 
 #define MAX_CODE 1024
 #define MAX_LABELS 100
+#define REGISTER_COUNT 3
 
 uint8_t bytecode[1024];
 int pc = 0;
 
 typedef struct {
-    const char* name;
-    uint8_t code;
+    	const char* name;
+   	uint8_t code;
 } Instruction;
 
 Instruction instructions[] = {
-    {"HALT", 0},
-    {"PUSH", 1},
-    {"DEL", 2},
-    {"ADD", 3},
-    {"SUB", 4},
-    {"MULT", 5},
-    {"DIV", 6},
-    {"PRINT", 7},
-    {"PEEK", 8},
-    {"JMP", 9},
-    {"JMP_IF_ZERO", 10},
-    {"JMP_IF_NOT_ZERO", 11},
-    {"EQ", 12},
-    {"NOT_EQ", 13},
-    {"LT", 14},
-    {"GT", 15},
-    {"LE", 16},
-    {"GE", 17},
-    {"DUP", 18},
-    {"SWAP", 19},
-    {"OVER", 20},
-    {"NOP", 21},
-    {"STORE", 22},
-    {"LOAD", 23}
+    	{"HALT", 0},
+    	{"PUSH", 1},
+    	{"DEL", 2},
+    	{"CLEAR", 3},
+    	{"ADD", 4},
+    	{"SUB", 5},
+    	{"MULT", 6},
+    	{"DIV", 7},
+    	{"PRINT", 8},
+    	{"ASCII", 9},
+    	{"PEEK", 10},
+    	{"JMP", 11},
+    	{"JMP_IF_ZERO", 12},
+    	{"JMP_IF_NOT_ZERO", 13},
+    	{"EQ", 14},
+    	{"NOT_EQ", 15},
+    	{"LT", 16},
+    	{"GT", 17},
+    	{"LE", 18},
+    	{"GE", 19},
+    	{"DUP", 20},
+    	{"SWAP", 21},
+    	{"OVER", 22},
+    	{"NOP", 23},
+    	{"STORE", 24},
+	{"LOAD", 25}
 };
 
+Instruction registers[] = {
+	{"R0", 0},
+	{"R1", 1},
+	{"R2", 2},
+	{"R3", 3},
+	{"R4", 4},
+	{"R5", 5},
+	{"R6", 6},
+	{"R7", 7}
+};
 
 int get_opcode(const char* name) {
-    for (int i = 0; i < (sizeof(instructions)/sizeof(instructions[0])); i++) {
-        if (strcmp(instructions[i].name, name) == 0) {
-            return instructions[i].code;
-        }
-    }
-    return -1;
+	for (int i = 0; i < (sizeof(instructions)/sizeof(instructions[0])); i++) {
+        	if (strcmp(instructions[i].name, name) == 0) {
+			return instructions[i].code;
+        	}
+    	}
+	return -1;
+}
+
+int check_for_register(const char* name) {
+	for (int i = 0; i < (sizeof(registers)/sizeof(registers[0])); i++) {
+		if (strcmp(registers[i].name, name) == 0) {
+			return registers[i].code;
+		}
+	}
+	return -1;
 }
 
 
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s input.asm output.obj\n", argv[0]);
-        return 1;
-    }
+    	if (argc < 3) {
+        	fprintf(stderr, "Usage: %s input.asm output.obj\n", argv[0]);
+        	return 1;
+    	}
 
-    const char* input_file = argv[1];
-    const char* output_file = argv[2];
+    	const char* input_file = argv[1];
+    	const char* output_file = argv[2];
 
-    FILE* file = fopen(input_file, "r");
-    if (!file) {
-        perror("Failed to open file");
-        return 1;
-    }
+    	FILE* file = fopen(input_file, "r");
+    	if (!file) {
+        	perror("Failed to open file");
+        	return 1;
+    	}
 
-    char line[128];
-    char* token;
+    	char line[128];
+    	char* token;
 
-    while (fgets(line, sizeof(line), file)) {
-        token = strtok(line, " \t\n");
-        while (token != NULL) {
-            int opcode = get_opcode(token);
-            if (opcode != -1) {
-                printf("Opcode: %d\n", opcode);
-                bytecode[pc] = opcode;
-            } else {
-                printf("Value or unknown instruction: %s\n", token);
-                bytecode[pc] = (uint8_t)atoi(token);
-            }
+    	while (fgets(line, sizeof(line), file)) {
+        	token = strtok(line, " \t\n");
+        	while (token != NULL) {
+            		int opcode = get_opcode(token);
+			int regcode = check_for_register(token);
+			if (opcode != -1) {
+                		printf("Opcode: %d\n", opcode);
+                		bytecode[pc] = opcode;
+            		} else if (regcode != -1) {
+				bytecode[pc] = regcode;
+			} else {
+                		printf("Value or unknown instruction: %s\n", token);
+                		bytecode[pc] = (uint8_t)atoi(token);
+            		}
 
-            token = strtok(NULL, " \t\n");
-            pc = pc + 1;
-        }
-    }
+            		token = strtok(NULL, " \t\n");
+            		pc = pc + 1;
+        	}
+    	}
 
-    fclose(file);
+    	fclose(file);
 
     
-    FILE* out = fopen(output_file, "wb");
-    int bytecode_length = sizeof(bytecode)/sizeof(bytecode[0]);
-    fwrite(bytecode, 1, bytecode_length, out);
-    fclose(out);
+    	FILE* out = fopen(output_file, "wb");
+    	int bytecode_length = sizeof(bytecode)/sizeof(bytecode[0]);
+    	fwrite(bytecode, 1, bytecode_length, out);
+    	fclose(out);
 
-    uint8_t bytecode[] = {1, 4, 18, 7, 0};  // Example bytecode
-    size_t length = sizeof(bytecode) / sizeof(bytecode[0]);
-    return 0;    
+    	uint8_t bytecode[] = {1, 4, 18, 7, 0};  // Example bytecode
+    	size_t length = sizeof(bytecode) / sizeof(bytecode[0]);
+    	return 0;    
 }
--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+
 
 // Define the instruction set
 typedef enum {
 	OP_HALT = 0,
     	OP_PUSH,     		// Push a constant onto the stack
     	OP_DEL,	 			// Delete latest constant of the stack
+	OP_CLEAR,				// Delete whole stack
     	OP_ADD,      		// Pop two values, add them, push result
     	OP_SUB,	 			// Pop two calues, subtract one from the other, push result
     	OP_MULT, 	 		// Pop two values, multiply them with each other, push result
     	OP_DIV,	 			// Pop two values, divide one through the other, push result
     	OP_PRINT,    		// Print top of stack and consume it
+	OP_ASCII,			// Print whole stack
     	OP_PEEK,	 		// Print top of stack without consuming it
     	OP_JMP,	 			// Jump to the instruction matching the given index
     	OP_JMP_IF_ZERO,		// Jump if top of stack does equal zero
@@ -21,13 +25,12 @@ typedef enum {
     	OP_GT,    			// Pop top two values, push 1 if greater than, else push 0
     	OP_LE,    			// Pop top two values, push 1 if greater than or equal, else push 0
     	OP_GE,    			// Pop top two values, push 1 if greater than or equal, else push 0
-		OP_DUP,				// Pop top value and push it twice
-		OP_SWAP,			// Swap top 2 values
-		OP_OVER,			// Copy second element on top 4, 8 -> 4, 8, 4
-		OP_NOP,				// Empty instruction
-
-		OP_STORE_R,			//Pop top value and store in given register address
-		OP_LOAD_R			//Pop value of given register address and load it onto the stack
+	OP_DUP,				// Pop top value and push it twice
+	OP_SWAP,			// Swap top 2 values
+	OP_OVER,			// Copy second element on top 4, 8 -> 4, 8, 4
+	OP_NOP,				// Empty instruction
+	OP_STORE_R,			//Pop top value and store in given register address
+	OP_LOAD_R			//Pop value of given register address and load it onto the stack
 } OpCode;
 
 // Stack setup
@@ -37,12 +40,30 @@ int sp = -1; // Stack pointer
 
 // Register setup
 #define REGISTER_SIZE 8
-uint16_t reg[REGISTER_SIZE]; // R0-R7
+uint16_t r0[REGISTER_SIZE];
+uint16_t r1[REGISTER_SIZE];
+uint16_t r2[REGISTER_SIZE];
+uint16_t r3[REGISTER_SIZE];
+uint16_t r4[REGISTER_SIZE];
+uint16_t r5[REGISTER_SIZE];
+uint16_t r6[REGISTER_SIZE];
+uint16_t r7[REGISTER_SIZE];
+
+typedef enum {
+	R0 = 0,
+	R1,
+	R2,
+	R3,
+	R4,
+	R5,
+	R6,
+	R7
+} RegCode;
 
 // Stack operations
 void push(int value) {
     	if (sp >= STACK_SIZE - 1) {
-        	printf("Stack overflow!\n");
+       		printf("Stack overflow!\n");
         	return;
     	}
     	stack[++sp] = value;
@@ -71,63 +92,77 @@ void run(uint8_t *program, size_t length) {
     	while (1) {
         	OpCode op = program[pc++];  // Fetch
 
-        	switch (op) {
-            	case OP_HALT: {
-            		return;
-				}
+      		switch (op) {
+            		case OP_HALT: {
+            			return;
+			}
 
-            	case OP_PUSH: {
-            		push(program[pc++]);
-            		break;
-				}
+            		case OP_PUSH: {
+            			push(program[pc++]);
+            			break;
+			}
 
 	    		case OP_DEL: {
 	    			int a = pop();
 				break;
 	    		}
 
-            	case OP_ADD: {
-            		int b = pop();
-            		int a = pop();
-            		push(a + b);
-            		break;
+			case OP_CLEAR: {
+				while (sp != -1) {
+					int a = pop();
+				}	 
+				break;
+			}
+		
+            		case OP_ADD: {
+            			int b = pop();
+            			int a = pop();
+            			push(a + b);
+            			break;
         		}
 
 	    		case OP_SUB: {
-					int b = pop();
-					int a = pop();
-					push(a - b);
-					break;		
+				int b = pop();
+				int a = pop();
+				push(a - b);
+				break;		
 	    		}
-
-	    		case OP_MULT: {
-					int b = pop();
-					int a = pop();
-					push(a * b);
-					break;		
+			
+			case OP_MULT: {
+				int b = pop();
+				int a = pop();
+				push(a * b);
+				break;		
 	    		}
 
 	    		case OP_DIV: {
-					int b = pop();
-					int a = pop();
-					if (b == 0) {
-						printf("You can NOT divide by 0.\nThis VM will shut down as means of a security measure!\n");
-						return;
-					}
-
-					push(a / b);		
-					break;
+				int b = pop();
+				int a = pop();
+				if (b == 0) {
+					printf("You can NOT divide by 0.\nThis VM will shut down as means of a security measure!\n");
+					return;
+				}
+	
+				push(a / b);		
+				break;
 	    		}
 
-            	case OP_PRINT: {
-                	printf("%d\n", pop());
-                	break;
-				}
+            		case OP_PRINT: {
+                		printf("%d", pop());
+                		break;
+			}
+
+			case OP_ASCII: {
+				int a = pop();
+				char ascii_char = (char)a;
+				printf("%c", ascii_char);
+				break;
+			}
 
 	    		case OP_PEEK: {
 				printf("%d\n", peek());
 				break;
-				}
+			}
 
 	    		case OP_JMP: {
            			if (pc >= length) {
@@ -137,7 +172,7 @@ void run(uint8_t *program, size_t length) {
 
     				pc = program[pc];  // Jump to absolute position
     				break;
-				}
+			}
 
 	    		case OP_JMP_IF_ZERO: {
            			if (pc >= length) {
@@ -145,15 +180,15 @@ void run(uint8_t *program, size_t length) {
         				return;
     				}
 
-					uint8_t target = program[pc++];
-					int condition = pop();
+				uint8_t target = program[pc++];
+				int condition = pop();
 
-					if (condition == 0) {
+				if (condition == 0) {
     					pc = target;
-					}
-
-					break;
 				}
+
+				break;
+			}
 
 	    		case OP_JMP_IF_NOT_ZERO: {
            			if (pc >= length) {
@@ -164,24 +199,24 @@ void run(uint8_t *program, size_t length) {
 				uint8_t target = program[pc++];
 				int condition = pop();
 
-					if (condition != 0) {
+				if (condition != 0) {
     					pc = target;
-					}
-
-					break;
 				}
 
-				case OP_EQ: {
-					int b = pop();	    
-					int a = pop();	    
-					if (a == b) {
-						push(1);
-					} else {
-						push(0);
-					}
+				break;
+			}
 
-				 	break;
+			case OP_EQ: {
+				int b = pop();	    
+				int a = pop();	    
+				if (a == b) {
+					push(1);
+				} else {
+					push(0);
 				}
+
+				break;
+			}
 
 			case OP_NOT_EQ: {
 				int b = pop();	    
@@ -273,15 +308,97 @@ void run(uint8_t *program, size_t length) {
 				break;
 			}
 
-				case OP_STORE_R: {
-					reg[program[pc++]] = pop();
-					break;
-				}
+			case OP_STORE_R: {
+				RegCode reg = program[pc++];
 
-				case OP_LOAD_R: {
-					push(reg[program[pc++]]);
-					break;
+				switch (reg) {
+					case R0: {
+						r0[program[pc++]] = pop();
+						break;
+					}
+					
+					case R1: {
+						r1[program[pc++]] = pop();
+						break;	 
+					}
+					
+					case R2: {
+						r2[program[pc++]] = pop();
+						break;	 
+					}
+					
+					case R3: {
+						r3[program[pc++]] = pop();
+						break;	 
+					}
+					
+					case R4: {
+						r4[program[pc++]] = pop();
+						break;	 
+					}
+					
+					case R5: {
+						r5[program[pc++]] = pop();
+						break;	 
+					}
+					
+					case R6: {
+						r6[program[pc++]] = pop();
+						break;	 
+					}
+					
+					case R7: {
+						r7[program[pc++]] = pop();
+						break;	 
+					}
 				}
+				break;
+			}
+
+			case OP_LOAD_R: {
+				switch (program[pc++]) {
+					case R0: {
+						push(r0[program[pc++]]);
+						break;
+					}
+					
+					case R1: {
+						push(r1[program[pc++]]);
+					      	break;		
+					}
+					
+					case R2: {
+						push(r2[program[pc++]]);
+						break;		
+					}
+					
+					case R3: {
+						push(r3[program[pc++]]);
+					       	break;		
+					}
+					
+					case R4: {
+						push(r4[program[pc++]]);
+					      	break;		
+					}
+					
+					case R5: {
+						push(r5[program[pc++]]);
+					       	break;		
+					}
+						
+					case R6: {
+						push(r6[program[pc++]]);
+					      	break;		
+					}
+						
+					case R7: {
+						push(r7[program[pc++]]);
+						break;		
+					}
+				}
+				break;
+			}
 
 			default: {
                 		printf("Unknown instruction: %d\n", op);
@@ -292,57 +409,35 @@ void run(uint8_t *program, size_t length) {
 }
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-
 uint8_t* load_bytecode(const char* filename, size_t* size_out) {
-    FILE* file = fopen(filename, "rb");
-    if (!file) {
-        perror("Failed to open .obj file");
-        exit(1);
-    }
+    	FILE* file = fopen(filename, "rb");
+    	if (!file) {
+        	perror("Failed to open .obj file");
+        	exit(1);
+    	}
 
-    fseek(file, 0, SEEK_END);
-    size_t size = ftell(file);
-    rewind(file);
+    	fseek(file, 0, SEEK_END);
+    	size_t size = ftell(file);
+    	rewind(file);
 
-    uint8_t* buffer = malloc(size);
-    if (!buffer) {
-        perror("Memory allocation failed");
-        exit(1);
-    }
+    	uint8_t* buffer = malloc(size);
+    	if (!buffer) {
+        	perror("Memory allocation failed");
+        	exit(1);
+    	}
 
-    fread(buffer, 1, size, file);
-    fclose(file);
+    	fread(buffer, 1, size, file);
+    	fclose(file);
 
-    *size_out = size;
-    return buffer;
+    	*size_out = size;
+    	return buffer;
 }
 
 int main(int argc, char* argv[]) {
-    size_t program_size;
-    uint8_t* program = load_bytecode(argv[1], &program_size);
+    	size_t program_size;
+    	uint8_t* program = load_bytecode(argv[1], &program_size);
 
-    run(program, program_size);  // Your VM's run function
-    free(program);
-    return 0;
+    	run(program, program_size);  // Your VM's run function
+    	free(program);
+    	return 0;
 }
-
-
-
-    	/*
-		uint8_t program[] = {
-			OP_PUSH, 8,
-			OP_DUP,
-			OP_DUP,
-			OP_MULT,
-			OP_PRINT,
-			OP_DUP,
-			OP_PUSH, 1,
-			OP_SUB,
-			OP_DUP,
-			OP_JMP_IF_NOT_ZERO, 2,
-			OP_HALT
-		};
-		*/
